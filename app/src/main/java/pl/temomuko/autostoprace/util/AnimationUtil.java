8f8eab7a665787dbed98498b3e25d28d3a56aee5
freszu <rafal.naniewicz@gmail.com>
2016-03-11 18:05:30 +0100

    Fixed animation flickering WOAH!

--- app/src/main/java/pl/temomuko/autostoprace/util/AnimationUtil.java
+++ app/src/main/java/pl/temomuko/autostoprace/util/AnimationUtil.java
@@ -17,20 +17,24 @@ public class AnimationUtil {
         throw new AssertionError();
     }
 
-    public static void animateTextViewMaxLinesChange(TextView textView, int maxLines, int duration) {
-        int startHeight = textView.getMeasuredHeight();
-        textView.setMaxLines(maxLines);
-        textView.measure(
-                View.MeasureSpec.makeMeasureSpec(textView.getWidth(), View.MeasureSpec.AT_MOST),
-                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-        int endHeight = textView.getMeasuredHeight();
-        ObjectAnimator animation = ObjectAnimator.ofInt(textView, MAX_HEIGHT_ATTR, startHeight, endHeight);
-        animation.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                textView.setMaxLines(maxLines);
-            }
-        });
-        animation.setDuration(duration).start();
+    public static void animateTextViewMaxLinesChange(final TextView textView, final int maxLines, final int duration) {
+        if (maxLines != textView.getLineCount()) {
+            final int startHeight = textView.getMeasuredHeight();
+            textView.setMaxLines(maxLines);
+            textView.measure(
+                    View.MeasureSpec.makeMeasureSpec(textView.getWidth(), View.MeasureSpec.EXACTLY),
+                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+            final int endHeight = textView.getMeasuredHeight();
+            ObjectAnimator animation = ObjectAnimator.ofInt(textView, MAX_HEIGHT_ATTR, startHeight, endHeight);
+            animation.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    if (textView.getMaxHeight() == endHeight) {
+                        textView.setMaxLines(maxLines);
+                    }
+                }
+            });
+            animation.setDuration(duration).start();
+        }
     }
 }
