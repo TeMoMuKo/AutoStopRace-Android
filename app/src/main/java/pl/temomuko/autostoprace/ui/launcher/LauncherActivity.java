611ddba7a663fbbc5c5845e0a3ab3a0aa7b531e3
szymenpl <me@szymen.pl>
2016-03-16 21:31:38 +0100

    Updated splash screen

--- app/src/main/java/pl/temomuko/autostoprace/ui/launcher/LauncherActivity.java
+++ app/src/main/java/pl/temomuko/autostoprace/ui/launcher/LauncherActivity.java
@@ -3,6 +3,9 @@ package pl.temomuko.autostoprace.ui.launcher;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
 import pl.temomuko.autostoprace.R;
@@ -17,13 +20,31 @@ public class LauncherActivity extends DrawerActivity {
 
     @Bind(R.id.btn_go_to_login) Button mGoToLoginButton;
     @Bind(R.id.btn_go_to_contact) Button mGoToContactButton;
+    @Bind(R.id.iv_launcher_logo) ImageView mAppLogoImageView;
+    @Bind(R.id.iv_launcher_bg) ImageView mBackgroundImageView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
         setupToolbarWithToggle();
+        loadAppLogo();
+        loadBackground();
         setListeners();
+    }
+
+    private void loadAppLogo() {
+        Picasso.with(this)
+                .load(R.drawable.logo)
+                .placeholder(R.drawable.logo)
+                .into(mAppLogoImageView);
+    }
+
+    private void loadBackground() {
+        Picasso.with(this)
+                .load(R.drawable.bg)
+                .placeholder(R.drawable.bg)
+                .into(mBackgroundImageView);
     }
 
     private void setListeners() {
