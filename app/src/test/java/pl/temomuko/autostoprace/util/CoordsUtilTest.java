f3b632a8199382e5a4c5fc110a9da007e9bd3b86
szymenpl <me@szymen.pl>
2016-04-17 20:17:55 +0200

    Code style improved

--- app/src/test/java/pl/temomuko/autostoprace/util/CoordsUtilTest.java
+++ app/src/test/java/pl/temomuko/autostoprace/util/CoordsUtilTest.java
@@ -15,8 +15,8 @@ public class CoordsUtilTest {
         String expected = "N 40°42’46”, W 74°0’21”";
         assertEquals(expected, actual);
 
-        expected = "N 51°6’28”, E 17°2’19”";
         actual = CoordsUtil.getDmsTextFromDecimalDegrees(51.1078852, 17.03853760000004);
+        expected = "N 51°6’28”, E 17°2’19”";
         assertEquals(expected, actual);
     }
 }
