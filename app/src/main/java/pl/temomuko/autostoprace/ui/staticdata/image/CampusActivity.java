ae882f0c05b21a608e305f73a051d879b488d757
Rafal Naniewicz <rafal.naniewicz@netguru.pl>
2017-04-27 21:16:03 +0200

    Remove created by

--- app/src/main/java/pl/temomuko/autostoprace/ui/staticdata/image/CampusActivity.java
+++ app/src/main/java/pl/temomuko/autostoprace/ui/staticdata/image/CampusActivity.java
@@ -2,9 +2,6 @@ package pl.temomuko.autostoprace.ui.staticdata.image;
 
 import pl.temomuko.autostoprace.R;
 
-/**
- * Created by Szymon Kozak on 2016-02-05.
- */
 public class CampusActivity extends FullscreenImageActivity {
     @Override
     protected int getImageId() {
