44d789d22c4bfa4d2cc31e0e8831a3220c58a080
szymen <szymon.kozak@droidsonroids.pl>
2018-04-15 22:41:27 +0200

    Remove unnecessary resources

--- app/src/main/java/pl/temomuko/autostoprace/data/local/LocationsViewMode.kt
+++ app/src/main/java/pl/temomuko/autostoprace/data/local/LocationsViewMode.kt
@@ -1,10 +1,5 @@
 package pl.temomuko.autostoprace.data.local
 
-import android.support.annotation.DrawableRes
-import android.support.annotation.StringRes
-import pl.temomuko.autostoprace.R
-
-enum class LocationsViewMode(@StringRes val titleRes: Int, @DrawableRes val iconRes: Int) {
-    MAP(R.string.mode_map, R.drawable.ic_map_white_24dp),
-    WALL(R.string.mode_wall, R.drawable.ic_view_list_black_24dp)
+enum class LocationsViewMode {
+    MAP, WALL
 }
