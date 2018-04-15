0a1a317a89fa6379b203fb981ba41e11a1f80690
Rafal Naniewicz <rafal.naniewicz@netguru.pl>
2018-04-16 00:01:14 +0200

    Add JvmStatic

--- app/src/main/java/pl/temomuko/autostoprace/ui/base/BaseActivity.java
+++ app/src/main/java/pl/temomuko/autostoprace/ui/base/BaseActivity.java
@@ -39,7 +39,7 @@ public abstract class BaseActivity extends AppCompatActivity {
         if (mActivityComponent == null) {
             mActivityComponent = DaggerActivityComponent.builder()
                     .activityModule(new ActivityModule(this))
-                    .applicationComponent(AsrApplication.Companion.getApplicationComponent(this))
+                    .applicationComponent(AsrApplication.getApplicationComponent(this))
                     .build();
         }
         return mActivityComponent;
