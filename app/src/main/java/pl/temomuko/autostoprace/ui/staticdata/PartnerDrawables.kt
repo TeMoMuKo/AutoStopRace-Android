042d4593678ad31480253852cedf2d2e637585ea
szymen <szymon.kozak@droidsonroids.pl>
2018-04-19 12:10:34 +0200

    Update partners

--- app/src/main/java/pl/temomuko/autostoprace/ui/staticdata/PartnerDrawables.kt
+++ app/src/main/java/pl/temomuko/autostoprace/ui/staticdata/PartnerDrawables.kt
@@ -10,33 +10,81 @@ object PartnerDrawables {
     )
 
     @JvmField val GOLD = listOf(
-        R.drawable.zloty_akvo,
-        R.drawable.zloty_decathlon,
+        R.drawable.zloty_wachitgall,
         R.drawable.zloty_hatchi,
-        R.drawable.zloty_luba,
         R.drawable.zloty_mamut,
+        R.drawable.zloty_akvo,
+        R.drawable.zloty_luba,
         R.drawable.zloty_sygnet,
-        R.drawable.zloty_wachitgall
+        R.drawable.zloty_decathlon
     )
 
     @JvmField val SILVER = listOf(
-        R.drawable.srebrny_beskidzkie,
-        R.drawable.srebrny_burgerking,
-        R.drawable.srebrny_findyourbuddy,
-        R.drawable.srebrny_kravmaga,
-        R.drawable.srebrny_lovex,
-        R.drawable.srebrny_narny,
         R.drawable.srebrny_profi,
+        R.drawable.srebrny_lovex,
         R.drawable.srebrny_snowz,
+        R.drawable.srebrny_kravmaga,
+        R.drawable.srebrny_burgerking,
+        R.drawable.srebrny_narny,
+        R.drawable.srebrny_beskidzkie,
+        R.drawable.srebrny_findyourbuddy,
         R.drawable.srebrny_tacoslocos
     )
 
+    @JvmField val BROWN = listOf(
+        R.drawable.brazowy_sonko,
+        R.drawable.brazowy_irenki,
+        R.drawable.brazowy_sandmix,
+        R.drawable.brazowy_funsurf,
+        R.drawable.brazowy_sjos,
+        R.drawable.brazowy_spocket,
+        R.drawable.brazowy_unicarcapoeira,
+        R.drawable.brazowy_setenta,
+        R.drawable.brazowy_gorczanska,
+        R.drawable.brazowy_yourtemporarytattoos,
+        R.drawable.brazowy_elsueno,
+        R.drawable.brazowy_inkblink
+    )
+
+    @JvmField val SUPPORTING = listOf(
+        R.drawable.wspierajacy_domekzkart,
+        R.drawable.wspierajacy_albi,
+        R.drawable.wspierajacy_lereve,
+        R.drawable.wspierajacy_wydrukujemyto,
+        R.drawable.wspierajacy_bezdroza,
+        R.drawable.wspierajacy_ksiegarniapodroznika,
+        R.drawable.wspierajacy_gojump,
+        R.drawable.wspierajacy_cudawianki,
+        R.drawable.wspierajacy_tkalniazagadek,
+        R.drawable.wspierajacy_mobicafe,
+        R.drawable.wspierajacy_hdprint,
+        R.drawable.wspierajacy_crashball,
+        R.drawable.wspierajacy_oriflame,
+        R.drawable.wspierajacy_samart,
+        R.drawable.wspierajacy_squash,
+        R.drawable.wspierajacy_zona71
+    )
+
+    @JvmField val HONORARY = listOf(
+        R.drawable.honorowy_uewroc,
+        R.drawable.honorowy_samorzad
+    )
+
+    @JvmField val TECHNOLOGICAL = listOf(
+        R.drawable.techniczny_smscenter,
+        R.drawable.techniczny_sendingo,
+        R.drawable.techniczny_damianmekal,
+        R.drawable.techniczny_paramed,
+        R.drawable.techniczny_mavmedia
+    )
+
     @JvmField val MEDIA = listOf(
-        R.drawable.mowia_o_nas_luz,
-        R.drawable.mowia_o_nas_meloradio,
         R.drawable.mowia_o_nas_nationalgeographic,
+        R.drawable.mowia_o_nas_eska,
+        R.drawable.mowia_o_nas_meloradio,
+        R.drawable.mowia_o_nas_luz,
         R.drawable.mowia_o_nas_radiogra
     )
 
-    @JvmStatic val splashPartners = STRATEGIC + GOLD + SILVER
+    @JvmStatic val splashPartners = STRATEGIC + GOLD + SILVER + BROWN + SUPPORTING + HONORARY + TECHNOLOGICAL
 }
